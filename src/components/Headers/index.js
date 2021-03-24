024bec5d13d4d54332781309e9261239db6b6019
Jean Diego <jeean.james@gmail.com>
2021-03-24 10:04:36 -0300

    ✨ New header component

---
+++ src/components/Headers/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './styles';
+
+import { Logo } from '~/svgs';
+
+const Headers = () => {
+  return (
+    <Container>
+      <Logo />
+    </Container>
+  );
+};
+
+export default Headers;
---
+++ src/components/Headers/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  flex-direction: row;
+  padding: 8px 16px;
+`;
