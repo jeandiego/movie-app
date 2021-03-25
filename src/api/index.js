46a9c414926708c3649df989e1b6998565e20807
Jean Diego <jeean.james@gmail.com>
2021-03-25 20:42:08 -0300

    🏗 Added api params

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,16 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
+
   timeout: 1000,
   headers: {
     method: 'GET',
     Authorization: `Bearer ${API_TOKEN}`,
     'Content-type': 'application/json',
+  },
+  params: {
+    language: 'pt-BR',
+    page: 1,
   },
 });
 
