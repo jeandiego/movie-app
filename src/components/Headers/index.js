ea842e679d1201dc49bb2574ca8c6a5071b8d07c
Jean Diego <jeean.james@gmail.com>
2021-03-25 20:39:51 -0300

    ✨ New header added

--- src/components/Headers/index.js
+++ src/components/Headers/index.js
@@ -1,14 +1,72 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import { Container } from './styles';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Gradient, HeaderWrapper, Text, PressArea } from './styles';
 
-import { Logo } from '~/svgs';
+import { Logo, ChevronRight, Back } from '~/svgs';
 
-const Headers = () => {
+const HeaderHome = () => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <Container>
-      <Logo />
-    </Container>
+    <Gradient
+      safeArea={insets}
+      colors={[
+        'rgba(23,23,33,1)',
+        'rgba(23,23,33,0.8)',
+        'rgba(23,23,33,0.6)',
+        'rgba(0,0,0,0)',
+      ]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}>
+      <Logo width={42} height={36} />
+    </Gradient>
   );
 };
 
-export default Headers;
+const HeaderPreview = () => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <Gradient
+      safeArea={insets}
+      colors={[
+        'rgba(23,23,33,1)',
+        'rgba(23,23,33,0.8)',
+        'rgba(23,23,33,0.6)',
+        'rgba(0,0,0,0)',
+      ]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}>
+      <PressArea>
+        <Back width={28} height={28} />
+      </PressArea>
+    </Gradient>
+  );
+};
+
+const HeaderList = ({ title }) => {
+  return (
+    <HeaderWrapper>
+      <Text>{title}</Text>
+      <ChevronRight />
+    </HeaderWrapper>
+  );
+};
+
+const Header = (props) => {
+  const { type } = props;
+
+  switch (type) {
+    case 'Home':
+      return <HeaderHome {...props} />;
+    case 'List':
+      return <HeaderList {...props} />;
+    case 'Preview':
+      return <HeaderPreview {...props} />;
+    default:
+      return <HeaderHome />;
+  }
+};
+
+export default Header;
--- src/components/Headers/styles.js
+++ src/components/Headers/styles.js
@@ -1,6 +1,34 @@
 import styled from 'styled-components';
+import LinearGradient from 'react-native-linear-gradient';
 
-export const Container = styled.View`
+export const Gradient = styled(LinearGradient)`
   flex-direction: row;
-  padding: 8px 16px;
+  padding: 16px;
+  padding-top: ${(props) => props.safeArea.top + 8}px;
+  position: absolute;
+  z-index: 10;
+  width: 100%;
+`;
+
+export const HeaderWrapper = styled.View`
+  width: 100%;
+  padding: 8px 16px 4px;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Text = styled.Text`
+  color: ${(props) => props.theme.text};
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+export const PressArea = styled.TouchableOpacity`
+  width: 44px;
+  height: 44px;
+  background: ${(props) => props.theme.semiTransparent};
+  border-radius: 22px;
+  justify-content: center;
+  align-items: center;
 `;
