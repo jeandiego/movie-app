8623a916d29534f1d964c8749497d2e0a2298844
Jean Diego <jeean.james@gmail.com>
2021-03-26 13:21:50 -0300

    🍱 Added new svgs

--- src/svgs/index.js
+++ src/svgs/index.js
@@ -4,3 +4,5 @@ export { default as Add } from './add';
 export { default as Send } from './send';
 export { default as Like } from './like';
 export { default as Back } from './arrow-left';
+export { default as LargeLogo } from './logo-large';
+export { default as LogoName } from './logo-name';
